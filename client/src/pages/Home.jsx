3a23816e43da7c31f6e80ec019dd9f9f191ee760
NimishP08 <113787407+NimishP08@users.noreply.github.com>
2024-01-31 21:09:51 +0530

    CONFLICTS RESOLVED

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div className='w-full h-full'>
-            Home Page
-        </div>
+    return (<>
+    <div>Home Page</div>
+       
+        </>
     );
 };
 
