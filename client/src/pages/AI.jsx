e3c5ffa565ce73a0647fc8992cc654dd48b68642
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2024-02-01 10:46:57 +0530

    ai gen added

--- client/src/pages/AI.jsx
+++ client/src/pages/AI.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect,useState } from 'react'
 import axios from "axios"
+import { toast } from 'react-toastify'
 
 function AI() {
     const [image, updateImage]= useState()
   const [prompt, updatePrompt]= useState()
 
   const generate = async () =>{
+    if (!prompt) {
+      toast.error('Prompt cannot be empty')
+      return
+    }
     const result = await axios.get(`http://127.0.0.1:8000/?prompt==${prompt}`)
     updateImage(result.data)
   }
   return (
-    <div className='relative w-full h-screen bg-zinc-800'>
-      <input value={prompt} onChange={e =>updatePrompt(e.target.value)}></input>
-      <button onClick = {e =>generate()}>GENERATE</button>
+    <div className='relative w-full h-screen flex flex-col items-center justify-start gap-8 mt-4'>
+      <div>
+        <h1 className='text-3xl font-bold'>Generate Thumbnails/Images with just a single Prompt!</h1>
+      </div>
+      <input className='border rounded-md p-2 w-1/2' placeholder='Enter a Prompt (Eg: Create a picture of a kid laughing)' value={prompt} onChange={e =>updatePrompt(e.target.value)}></input>
+      <button className='border p-2 rounded-md font-bold tracking-wide text-lg bg-l-blue text-white hover:bg-d-blue active:bg-l-blue' onClick = {e =>generate()}>GENERATE</button>
       {image ? (<img src={`data:image/png;base64,${image}`} />) : null}
     </div>
   )
